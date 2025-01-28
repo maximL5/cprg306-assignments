d69e24bb1214ea4b44027f8d9d941066fb79950d
maximL5 <maxim.lopez@edu.sait.ca>
2025-01-27 23:02:20 -0700

    Finished week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Maxim Lopez</p>
+            <Link href="https://github.com/maximL5/cprg306-assignments" >https://github.com/maximL5/cprg306-assignments</Link>
+        </div>
+    );
+}
